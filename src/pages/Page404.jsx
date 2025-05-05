8f555b8aee2e6cc274e366503409402a7f10549d
Team <teammembers@qadigitalads.com>
2025-05-05 11:31:25 -0500

    Update Page404.jsx

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -7,7 +7,7 @@ const Page404 = () => {
     <h1 className="text-qablack font-bold lg:text-[63px] text-[36px] pb-4 lg:leading-[75px] leading-[42px] px-8 lg:px-0">404</h1>
     <h2 className="text-qablack lg:text-[25px] text-[18px] font-light pb-4 px-8 lg:px-0"> Oops! Page Not Found</h2>
     <p className="text-qablack">
-    Sorry, the page you're looking for doesn't exist or has been moved.
+    Sorry, the page you're looking for doesn't exist or has been moved. 
     </p>
     </div>
       
